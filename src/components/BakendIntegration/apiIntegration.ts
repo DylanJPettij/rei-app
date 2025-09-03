16bb391df5418a694852545770cf2d9332bf8df5
Dylan Pettijohn <dylanpettijohn@Dylans-Mac-mini.local>
2025-09-03 16:59:05 -0400

    updating API Path

--- src/components/BakendIntegration/apiIntegration.ts
+++ src/components/BakendIntegration/apiIntegration.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const apiClient = axios.create({
-  baseURL: "http://18.188.87.230:8080",
+  baseURL: "api.reicalculator.net:8080",
   headers: {
     "Content-Type": "application/json",
   },
