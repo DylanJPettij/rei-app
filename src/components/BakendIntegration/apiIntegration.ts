cdb693a9f1cddecf952d44ecde52fb036d9acce2
Dylan Pettijohn <dylanpettijohn@Dylans-Mac-mini.local>
2025-09-02 23:42:07 -0400

    changing where apiClient file is pointing to AWS instance

--- src/components/BakendIntegration/apiIntegration.ts
+++ src/components/BakendIntegration/apiIntegration.ts
@@ -1,11 +1,9 @@
-import axios from 'axios'
+import axios from "axios";
 const apiClient = axios.create({
-    
-    
-    baseURL: 'http://localhost:8080',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: "http://18.188.87.230:8080",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiClient;
